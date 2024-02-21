581b505186f862172df2ce31a5ca3d3d36ddd328
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-21 10:16:33 -0700

    Card cleanup

--- src/components/CardFooter/CardFooter.jsx
+++ src/components/CardFooter/CardFooter.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Card as CardM } from 'react-bootstrap'
 /**
  * @uxpindocurl https://react-bootstrap.github.io/docs/components/Card
- * @uxpindescription TODO
+ * @uxpindescription Bootstrap’s cards provide a flexible and extensible content container with multiple variants and options.
  * @uxpinnamespace Card
  */
 const Footer = (props) => {
--- src/components/CardGroup/CardGroup.jsx
+++ src/components/CardGroup/CardGroup.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { CardGroup as CardGroupM } from 'react-bootstrap'
 /**
  * @uxpindocurl https://react-bootstrap.github.io/docs/components/Card
- * @uxpindescription TODO
+ * @uxpindescription Bootstrap’s cards provide a flexible and extensible content container with multiple variants and options.
  * @uxpinnamespace Card
  */
 const CardGroup = (props) => {
