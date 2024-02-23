cc238bbdb226b61eee89fc8a4c1c92c08c2859f0
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-22 18:40:58 -0700

    cleanup

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -30,7 +30,7 @@ const Modal = (props) => {
       }}
     >
       <div ref={containerRef}>
-        <ModalM {...props} container={containerRef} backdrop={uxpContainer ? true : false} />
+        <ModalM {...props} container={containerRef} backdrop={uxpContainer ? true : false} enforceFocus={false} />
       </div>
     </div>
   )
@@ -53,14 +53,18 @@ Modal.propTypes = {
   /** Override styles */
   style: PropTypes.object,
 
-  /** A Component type that provides the modal content Markup. This is a usefulprop when you want to use your own styles and markup to create a custommodal component. */
+  /** A Component type that provides the modal content Markup. This is a usefulprop when you want to use your own styles and markup to create a custommodal component.
+   * @uxpinignoreprop
+   */
   dialogAs: PropTypes.string,
   /** When `true` The modal will show itself.
    */
   show: PropTypes.bool,
   /** Open and close the Modal with a slide and fade animation. */
   animation: PropTypes.bool,
-  /** Include a backdrop component. Specify 'static' for a backdrop that doesn'trigger an "onHide" when clicked. */
+  /** Include a backdrop component. Specify 'static' for a backdrop that doesn'trigger an "onHide" when clicked.
+   * @uxpinignoreprop
+   */
   backdrop: PropTypes.string,
   /** Close the modal when escape key is pressed */
   keyboard: PropTypes.bool,
@@ -68,7 +72,9 @@ Modal.propTypes = {
   autoFocus: PropTypes.bool,
   /** When `true` The modal will prevent focus from leaving the Modal whileopen. Consider leaving the default value here, as it is necessary to makethe Modal work well with assistive technologies, such as screen readers. */
   enforceFocus: PropTypes.bool,
-  /** When `true` The modal will restore focus to previously focused element oncemodal is hidden */
+  /** When `true` The modal will restore focus to previously focused element oncemodal is hidden
+   * @uxpinignoreprop
+   */
   restoreFocus: PropTypes.bool,
   /** @default 'modal' */
   bsPrefix: PropTypes.string,
@@ -86,7 +92,9 @@ Modal.propTypes = {
   dialogClassName: PropTypes.string,
   /** Add an optional extra class name to .modal-content */
   contentClassName: PropTypes.string,
-  /** Options passed to focus function when `restoreFocus` is set to `true`@link  https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/focus#Parameters */
+  /** Options passed to focus function when `restoreFocus` is set to `true`@link  https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/focus#Parameters
+   * @uxpinignoreprop
+   */
   restoreFocusOptions: PropTypes.shape,
   /** A callback fired when the Modal is opening. */
   onShow: PropTypes.func,
@@ -106,9 +114,13 @@ Modal.propTypes = {
   onExiting: PropTypes.func,
   /** Callback fired after the Modal finishes transitioning out */
   onExited: PropTypes.func,
-  /** A ModalManager instance used to track and manage the state of openModals. Useful when customizing how modals interact within a container */
+  /** A ModalManager instance used to track and manage the state of openModals. Useful when customizing how modals interact within a container
+   * @uxpinignoreprop
+   */
   manager: PropTypes.object,
-  /** @private */
+  /** @private
+   * @uxpinignoreprop
+   */
   container: PropTypes.string,
   'data-bs-theme': PropTypes.string,
   'aria-labelledby': PropTypes.string,
