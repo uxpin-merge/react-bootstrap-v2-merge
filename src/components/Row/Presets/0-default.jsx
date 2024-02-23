f9b526018b73310e3fe02c3e8ce0fcff57b8803b
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-23 12:37:37 -0700

    cleanup

--- src/components/Row/Presets/0-default.jsx
+++ src/components/Row/Presets/0-default.jsx
@@ -5,19 +5,14 @@ import Card from '../../Card/Card'
 
 export default (
   <Row uxpId="row-1" className={'g-2'}>
-    <Col uxpId="col-1">
+    <Col uxpId="col-1" xs={3}>
       <Card body={true} uxpId="card-1">
-        1 of 3
+        3 columns
       </Card>
     </Col>
-    <Col uxpId="col-2">
+    <Col uxpId="col-2" xs={9}>
       <Card body={true} uxpId="card-2">
-        2 of 3
-      </Card>
-    </Col>
-    <Col uxpId="col-3">
-      <Card body={true} uxpId="card-3">
-        3 of 3
+        9 columns
       </Card>
     </Col>
   </Row>
