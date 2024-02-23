13d9db5021a1fecf8eaa3f1120844eb4a45b0947
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-23 16:42:00 -0700

    Form defaults

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -34,12 +34,11 @@ Form.propTypes = {
 
   /** Override styles */
   style: PropTypes.object,
-
+  /** Mark a form as having been validated. Setting it to `true` willtoggle any validation styles on the forms elements. */
+  validated: PropTypes.bool,
   as: PropTypes.string,
   /** The Form `ref` will be forwarded to the underlying element,which means, unless it's rendered `as` a composite component,it will be a DOM node, when resolved.@type {ReactRef}@alias ref */
   _ref: PropTypes.string,
-  /** Mark a form as having been validated. Setting it to `true` willtoggle any validation styles on the forms elements. */
-  validated: PropTypes.bool,
 }
 
 export default Form
--- src/components/Form/Presets/0-default.jsx
+++ src/components/Form/Presets/0-default.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import Form from '../Form';
+import React from 'react'
+import Form from '../Form'
+import Button from '../../Button/Button'
 
 export default (
-  <Form uxpId='form-1'>
-    Content here
+  <Form uxpId="form-1">
+    <Form.Group className="mb-3" controlId="formBasicEmail" uxpId="form-group-1">
+      <Form.Label uxpId="form-label-1">Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" uxpId="form-control-1" />
+      <Form.Text className="text-muted" uxpId="form-text-1">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicPassword" uxpId="form-group-2">
+      <Form.Label uxpId="form-label-2">Password</Form.Label>
+      <Form.Control uxpId="form-control-2" type="password" placeholder="Password" />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formBasicCheckbox" uxpId="form-group-3">
+      <Form.Check type="checkbox" label="Check me out" uxpId="form-check-1" />
+    </Form.Group>
+    <Button variant="primary" type="submit" uxpId="form-button-1">
+      Submit
+    </Button>
   </Form>
-);
+)
