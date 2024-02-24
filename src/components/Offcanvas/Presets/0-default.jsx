346b572f2c1517da392370ba435b46635173c1d4
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-02-24 16:04:37 -0700

    cleanup

--- src/components/Offcanvas/Presets/0-default.jsx
+++ src/components/Offcanvas/Presets/0-default.jsx
@@ -7,7 +7,9 @@ import OffcanvasBody from '../../OffcanvasBody/OffcanvasBody'
 export default (
   <Offcanvas show uxpId="offcanvas-1">
     <OffcanvasHeader closeButton uxpId="offcanvas-header-1">
-      <OffcanvasTitle uxpId="offcanvas-title-1">Offcanvas Title</OffcanvasTitle>
+      <OffcanvasTitle uxpId="offcanvas-title-1" as="h5">
+        Offcanvas Title
+      </OffcanvasTitle>
     </OffcanvasHeader>
     <OffcanvasBody uxpId="offcanvas-body-1">
       Content for the offcanvas goes here. You can place just about any Bootstrap component or custom elements here.
